6fb7ddf35c24dd8aa8693e0773ffa76eb903a579
Robert David Hernandez <robhernandez@microsoft.com>
2024-02-14 16:25:03 -0600

    add some thread safety for observer pattern

--- app/observer/ConcreteSubscriber.cs
+++ app/observer/ConcreteSubscriber.cs
@@ -1,13 +1,29 @@
 class ConcreteSubscriber : AbstractSubscriber
 {
+    private object _call_count_lock = new();
+    private int callCount = 0;
     private readonly string _id = Guid.NewGuid().ToString();
     public override void Update()
     {
-        Console.WriteLine("Update called on ConcreteSubscriber: " + _id);
+        incrementCallCount();
+        Console.WriteLine("Update #"+callCount+" called on ConcreteSubscriber: " + _id);
+    }
+
+    public override void Update(string[] data)
+    {
+        incrementCallCount();
+        base.Update(data);
+        Console.WriteLine("Data updated on ConcreteSubscriber: " + _id);
+        Console.WriteLine("Update #"+callCount+" called on ConcreteSubscriber: " + _id);
     }
     public override string ToString() {
         _data ??= [];
         string message = "ConcreteSubscriber: " + _id + " " + "Data: " + string.Join("", _data) + "\n";
         return message;
+    }
+    private void incrementCallCount() {
+        lock (_call_count_lock) {
+            callCount++;
+        }
     }
 }
